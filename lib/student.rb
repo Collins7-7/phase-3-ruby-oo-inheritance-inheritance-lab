b6e4e624c244404e9f7384fb34bd3eda5ba49de6
COllins <collinsmuiruri77@gmail.com>
2022-10-24 20:03:14 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user.rb'
+
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(brains)
+        @knowledge << brains
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
